7b65e8126addfeb5bbbb6a3be7d9a8cfddf7edf4
Nic Chan <channicoleelise@gmail.com>
2021-03-10 16:46:29 +0800

    Fix build

--- utils/transforms/htmlmin.js
+++ utils/transforms/htmlmin.js
@@ -1,15 +1,15 @@
-const htmlmin = require('html-minifier')
+const htmlmin = require("html-minifier");
 
-module.exports = function(content, outputPath) {
-  if (outputPath.endsWith('.html')) {
+module.exports = function (content, outputPath) {
+  if (outputPath && outputPath.endsWith(".html")) {
     let minified = htmlmin.minify(content, {
       useShortDoctype: true,
       removeComments: true,
       collapseWhitespace: true,
       minifyCSS: true,
-    })
-    return minified
+    });
+    return minified;
   }
 
-  return content
-}
+  return content;
+};
