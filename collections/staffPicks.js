c15db5a9ab0cae3be34c91944034ad892415ca02
Nic Chan <channicoleelise@gmail.com>
2021-03-25 09:44:24 +0800

    Only show staff picks that have been published

--- collections/staffPicks.js
+++ collections/staffPicks.js
@@ -1,7 +1,12 @@
 const pathConfig = require("../src/_data/paths.json");
 
+const now = new Date();
+const livePosts = (post) => post.date <= now && !post.data.draft;
+
 module.exports = (collection) => {
   return [
-    ...collection.getFilteredByGlob(`./${pathConfig.src}/staff-picks/**/*.md`),
-  ].sort();
+    ...collection
+      .getFilteredByGlob(`./${pathConfig.src}/staff-picks/**/*.md`)
+      .filter(livePosts),
+  ].reverse();
 };
