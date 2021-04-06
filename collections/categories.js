8d453092afa26ca31a570f8b5fa014493ccbe84f
Nic Chan <channicoleelise@gmail.com>
2021-04-06 14:47:09 +0800

    Sort authors and categories

--- collections/categories.js
+++ collections/categories.js
@@ -3,5 +3,7 @@ const pathConfig = require("../src/_data/paths.json");
 module.exports = (collection) => {
   return [
     ...collection.getFilteredByGlob(`./${pathConfig.src}/categories/**/*.md`),
-  ].sort();
+  ].sort(function (a, b) {
+    return a.data.name.localeCompare(b.data.name);
+  });
 };
